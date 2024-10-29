7735d7d69418d1242cc521537d309c5865c6124a
lara <struck08@ads.uni-passau.de>
2024-10-29 15:26:53 +0100

    Main method and package IoTFleetManagement added and is running.

---
+++ src/main/java/IoTFleetManagement/LoginApp.java
@@ -0,0 +1,19 @@
+package IoTFleetManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main application class for the Login module of the IoT Fleet Management System.
+ * This class serves as the entry point for the Spring Boot application.
+ *
+ * @author struckmeie
+ * @author jasmin1707
+ */
+@SpringBootApplication
+public class LoginApp {
+    public static void main(String[] args) {
+        // Launches the Spring Boot application
+        SpringApplication.run(LoginApp.class, args);
+    }
+}
