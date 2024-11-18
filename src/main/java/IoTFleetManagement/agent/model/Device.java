062a061c8fcec3f8c883faf8e96d2c407d5114a4
Jasmin1707 <sadegh02@ads.uni-passau.de>
2024-11-18 14:08:53 +0100

    Agent Api created. Co-authored-by: lara <struck08@ads.uni-passau.de>

--- src/main/java/IoTFleetManagement/agent/model/Device.java
+++ src/main/java/IoTFleetManagement/agent/model/Device.java
@@ -27,4 +27,59 @@ public class Device {
     private int pingFrequency;
 
     private String deviceType;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getDeviceId() {
+        return deviceId;
+    }
+    public void setDeviceId(String deviceId) {
+        this.deviceId = deviceId;
+    }
+    public String getSecretKey() {
+        return secretKey;
+    }
+    public void setSecretKey(String secretKey) {
+        this.secretKey = secretKey;
+    }
+    public LocalDateTime getLastSeen() {
+        return lastSeen;
+    }
+    public void setLastSeen(LocalDateTime lastSeen) {
+        this.lastSeen = lastSeen;
+    }
+    public String getStatus() {
+        return status;
+    }
+    public void setStatus(String status) {
+        this.status = status;
+    }
+    public String getToken() {
+        return token;
+    }
+    public void setToken(String token) {
+        this.token = token;
+    }
+    public String getFirmwareVersion() {
+        return firmwareVersion;
+    }
+    public void setFirmwareVersion(String firmwareVersion) {
+        this.firmwareVersion = firmwareVersion;
+    }
+    public int getPingFrequency() {
+        return pingFrequency;
+    }
+    public void setPingFrequency(int pingFrequency) {
+        this.pingFrequency = pingFrequency;
+    }
+    public String getDeviceType() {
+        return deviceType;
+    }
+    public void setDeviceType(String deviceType) {
+        this.deviceType = deviceType;
+    }
 }
