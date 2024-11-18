bf490f123530df50e0a4f7404bad8fba0f372c34
Jasmin1707 <sadegh02@ads.uni-passau.de>
2024-11-18 14:00:16 +0100

    added entity class. Co-authored-by: lara <struck08@ads.uni-passau.de>

--- src/main/java/IoTFleetManagement/agent/model/Device.java
+++ src/main/java/IoTFleetManagement/agent/model/Device.java
@@ -1,4 +1,30 @@
 package IoTFleetManagement.agent.model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
 public class Device {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String deviceId;
+
+    @Column(nullable = false)
+    private String secretKey;
+
+    private LocalDateTime lastSeen;
+
+    private String status;
+
+    private String token;
+
+    private String firmwareVersion;
+
+    private int pingFrequency;
+
+    private String deviceType;
 }
