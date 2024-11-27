107605ad6282f99cf66681774d9167c0ff7a554e
lara <struck08@ads.uni-passau.de>
2024-11-27 11:59:37 +0100

    Method for Jwt is token valid in JwtUtil.java and Agent Service Co-authored-by: Jasmin1707 <sadegh02@ads.uni-passau.de>

--- src/main/java/IoTFleetManagement/security/config/JwtUtil.java
+++ src/main/java/IoTFleetManagement/security/config/JwtUtil.java
@@ -4,10 +4,13 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
+@Component
 public class JwtUtil {
     private static final SecretKey SECRET_KEY = Keys.secretKeyFor(SignatureAlgorithm.HS512); // Replace with a secure key in production
 
@@ -18,7 +21,7 @@ public class JwtUtil {
                 .compact();
     }
 
-    public static boolean validateToken(String token, String agentId) {
+    public boolean validateToken(String token, String agentId) {
         try {
             Claims claims = Jwts.parser()
                     .setSigningKey(SECRET_KEY)
