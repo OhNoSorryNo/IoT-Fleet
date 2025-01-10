9240ed360a82eff5af2584c73c63521b246ce475
lara <struck08@ads.uni-passau.de>
2025-01-10 17:34:34 +0100

    update-check is extended to ask if update is needed

--- src/main/java/IoTFleetManagement/agent/controller/AgentController.java
+++ src/main/java/IoTFleetManagement/agent/controller/AgentController.java
@@ -342,7 +342,7 @@ public class AgentController {
 
             FirmwareVersion latestFirmware = firmwareVersionService.getLatestFirmwareVersion();
 
-            boolean updateRequired = !agent.getFirmwareVersion().equals(latestFirmware.getVersion());
+            boolean updateRequired = !agent.getFirmwareVersion().equals(latestFirmware.getVersion())&&agentService.isUpdateAgentUpdateNeeded(Long.valueOf(agentId)) ;
 
             return ResponseEntity.ok(Map.of(
                     "status", updateRequired,
