0bb6d752140d74d51e7eac380b025c130fa13a03
lara <struck08@ads.uni-passau.de>
2025-01-10 17:27:46 +0100

    Feedback implemented as far as possible and Status of UpdateNeeded is reset to false

--- Device/Communicator/src/main/java/service/SimulatedDeviceService.java
+++ Device/Communicator/src/main/java/service/SimulatedDeviceService.java
@@ -303,6 +303,7 @@ public class SimulatedDeviceService implements ApplicationListener<ApplicationRe
 
         Map<String, Object> requestBody = new HashMap<>();
         requestBody.put("status", success ? "success" : "failure");
+        requestBody.put("deviceId", simulatedDevice.getDeviceId()); // Device ID hinzufügen
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
@@ -320,7 +321,5 @@ public class SimulatedDeviceService implements ApplicationListener<ApplicationRe
             logger.error("Error sending update status: {}", e.getMessage());
         }
     }
-
-
 
 }
