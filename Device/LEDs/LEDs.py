1108df1f3da20d24952563c9868036f964eaab92
lara <struck08@ads.uni-passau.de>
2025-01-16 16:19:03 +0100

    LEDs.py and LEDs.iml created
    Co-authored-by: Jasmin1707 <sadegh02@ads.uni-passau.de>

---
+++ Device/LEDs/LEDs.py
@@ -0,0 +1,34 @@
+
+from flask import Flask, request
+from gpiozero import LED
+
+# LED-Setup
+green_led = LED(17)  # Grün: Heartbeat successful
+red_led = LED(27)    # Red: Heartbeat unsuccessful
+blue_led = LED(22)   # Blue: Device updating
+
+app = Flask(__name__)
+
+@app.route('/led', methods=['POST'])
+def control_led():
+    data = request.json
+    status = data.get("status")
+
+    green_led.off()
+    red_led.off()
+    blue_led.off()
+
+    # Status check and LED control
+    if status == "online":
+        green_led.on()
+    elif status == "offline":
+        red_led.on()
+    elif status == "updating":
+        blue_led.on()
+    else:
+        return "Invalid status", 400
+
+    return "LED status updated", 200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=6060)
